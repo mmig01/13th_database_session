0f7268fad8a711125ddb314b83ca97f3f51dc9b9
sunwoo030616 <sunwoo030616@dgu.ac.kr>
2025-04-06 10:32:01 +0900

    feat: Shortcuts 모델 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ShortKeys(models.Model):
+    key_name = models.TextField()
+
+    def __str__(self):
+        return self.key_name
+    #migration은 모델 필드나 db스키마 변경이 있을때만 필요
