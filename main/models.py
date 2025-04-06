7874b800ef6f75246e28635f53e4977d1525fbf9
sunwoo030616 <sunwoo030616@dgu.ac.kr>
2025-04-06 10:46:31 +0900

    feat: Person 모델 추가

--- main/models.py
+++ main/models.py
@@ -7,3 +7,10 @@ class ShortKeys(models.Model):
     def __str__(self):
         return self.key_name
     #migration은 모델 필드나 db스키마 변경이 있을때만 필요
+
+class Person(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+
+    def __str__(self):
+        return self.last_name
