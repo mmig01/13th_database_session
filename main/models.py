4417c3a4ae879772e914e0d22394e23685be0859
ymkim030403@gmail.com <ymkim030403@gmail.com>
2025-04-09 19:54:16 +0900

    feat: Shortcuts 모델 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Shortkeys(models.Model):
+    # 단축키 이름을 나타내기 위한 필드
+    key_name = models.TextField()
+
+    def __str__(self):
+        return self.key_name
