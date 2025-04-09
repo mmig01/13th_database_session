75823cfda80cb9ac08b647bb6d52ef4a0b03859b
onowoey <cg10046@naver.com>
2025-04-09 20:11:02 +0900

    3주차 과제

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import ShortKeys
+from .models import Person
 
 admin.site.register(ShortKeys)
+admin.site.register(Person)
